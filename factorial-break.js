a873169602541b2845db1e4d24d44094a306afee
EDavidGZ <jgar05202415@gmail.com>
2023-01-06 23:31:26 -0600

    three

---
+++ factorial-break.js
@@ -0,0 +1,10 @@
+let i = 1;
+let n = 1;
+while(i <= 10){
+    if(i == 11){
+        break;
+    }
+    n = n * i;
+    console.log(n);
+    i++;
+}
---
+++ factorial-while.js
@@ -0,0 +1,7 @@
+let i = 1;
+let n = 1;
+while(i <= 10){
+    n = n * i;
+    console.log(n);
+    i++;
+}
