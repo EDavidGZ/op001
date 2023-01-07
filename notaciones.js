aaf227ba22c49f0ff6b1627b74023c8606004dd2
EDavidGZ <jgar05202415@gmail.com>
2023-01-06 19:42:22 -0600

    second

---
+++ notaciones.js
@@ -0,0 +1,11 @@
+let name = 'Efren';
+let edad = 20;
+let developer = true;
+let birthDate = new Date("august 13 2002")
+const littlePrince = {
+    title: "little Prince",
+    author: "Antoine ofSaint-Exupéry",
+    date: new Date("april 12 1998"),
+    url: "http://bibliotecadigital.ilce.edu.mx/Colecciones/ObrasClasicas/_docs/ElPrincipito.pdf"
+}
+console.log(littlePrince)
