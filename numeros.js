06238618a1d4d66cb3bae524cceb000d43c4bf4e
EDavidGZ <jgar05202415@gmail.com>
2023-01-09 20:24:38 -0600

    five

---
+++ numeros.js
@@ -0,0 +1,7 @@
+let altura = 160
+let alturaF = 1.60 
+let peso = 57.1
+let exAl = Math.ceil(alturaF) 
+let exPe = Math.round(peso)
+let maximo = "el máximo valor que se puede obtener en Javascript + 1" = "máximo valor que se puede obtener en Javascript"
+console.log(exPe)
